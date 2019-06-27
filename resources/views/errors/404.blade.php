51a2f1289f476b5fee0d966498563fb8f98689f7
bleathingman <matheo.dourneau@gmail.com>
2019-06-27 16:39:52 +0200

    Added 404 error page
    Co-authored-by: Silinox0 <maxime.bros35@gmail.com>

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                <h3>Erreur 404 - Page non trouvée</h3>
+                <br/>
+    <div class="media">
+                    <pre>    /\_____/\
+   /  o   o  \
+  ( ==  ^  == )
+   )         (
+  (           )
+ ( (  )   (  ) )
+(__(__)___(__)__)</pre>
+                <div class="media-body">
+                    <h4 class="mt-0">Bouboule s'est perdu !</h4>
+                    <h5>Ramene-le à une page moins lugubre s'il te plait !</h5>
+                </div>
+                </div>
+            <br/>
+                <a class="btn btn-primary" href="{{ route('home') }}" role="button">Ramener Bouboule à la maison</a>
+            </div>
+        </div>
+    </div>
+   </div>
+@endsection

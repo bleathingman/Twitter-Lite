873c77b7bc58b82df5ed20980d7e012684d04ef0
bleathingman <matheo.dourneau@gmail.com>
2019-06-28 15:50:51 +0200

    typo fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -18,7 +18,7 @@
 (__(__)___(__)__)</pre>
                 <div class="media-body">
                     <h4 class="mt-0">Bouboule s'est perdu !</h4>
-                    <h5>Ramene-le à une page moins lugubre s'il te plait !</h5>
+                    <h5>Ramene-le à la maison s'il te plait !</h5>
                 </div>
                 </div>
             <br/>

08cdcd1265747a0ccffe2839cca667084f688467
Silinox0 <maxime.bros35@gmail.com>
2019-07-05 16:42:35 +0200

    Dernier commit du stage :C

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,8 @@
                         <h5>Ce site est le projet de stage de Silinox et Bleathingman, réaliser du 3 juin jusqu'au 5
                             juillet
                             2019.</h5>
-                        <h5>Site web : <a href="http://twitter-lite.localtunnel.me">http://twitter-lite.localtunnel.me</a></h5>
+                        <h5>Site web : <a
+                                href="http://twitter-lite.localtunnel.me">http://twitter-lite.localtunnel.me</a></h5>
                         <br />
                         <h5>- Réaliser avec le framework <a href="https://laravel.com/">Laravel 5.8</a></h5>
                         <h5>- Coder avec <a href="https://code.visualstudio.com/">Visual Studio Code</a></h5>
@@ -25,12 +26,13 @@
                         <h5>- Serveur de déploiment : <a href="https://localtunnel.github.io/www/">Localtunnel</a></h5>
                         <br>
                         <h5>Un grand merci à notre maître de stage sans qui tout cela aurait été impossible !<h5>
-                        <br/>
-                        <a href="https://github.com/Silinox0/Twitter-Lite" class="btn btn-primary">Git Hub</a>
-                        <br/>
-                        <br/>
-                        <h5>Serveur discord des développeur (Venez c'est gratuit) :</h5>
-                        <a href="https://discord.gg/DkZMXgS" class="btn btn-primary">Silinox</a> &nbsp; <a href="https://discord.gg/eRuY36F" class="btn btn-primary">Bleathingman</a>
+                                <br />
+                                <a href="https://github.com/Silinox0/Twitter-Lite" class="btn btn-primary">Git Hub</a>
+                                <br />
+                                <br />
+                                <h5>Serveur discord des développeurs (Venez c'est gratuit) :</h5>
+                                <a href="https://discord.gg/DkZMXgS" class="btn btn-primary">Silinox</a> &nbsp; <a
+                                    href="https://discord.gg/eRuY36F" class="btn btn-primary">Bleathingman</a>
                     </div>
                 </div>
             </div>
